89dbbaf6b75f74318380e1d69b3ed1369f83447e
eben4ya <benaya.imanuela@gmail.com>
2024-11-12 00:03:52 +0700

    <feat: added route to bayar sekarang button at payment page>

--- Appview/Views/Payment.xaml.cs
+++ Appview/Views/Payment.xaml.cs
@@ -24,5 +24,16 @@ namespace Appview.Views
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var loginPage = new RecommendedFood();
+            var mainWindow = Application.Current.MainWindow as MainWindow;
+
+            if (mainWindow != null)
+            {
+                mainWindow.Content = loginPage;
+            }
+        }
     }
 }
