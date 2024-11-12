75f17c4f247c3de3ada2a8a24e739005b2596c33
eben4ya <benaya.imanuela@gmail.com>
2024-11-12 14:04:35 +0700

    <feat: added add product function in AddProduct.xaml.cs>

--- Appview/Views/AddProducts.xaml.cs
+++ Appview/Views/AddProducts.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,6 +35,41 @@ namespace Appview.Views
             if (mainWindow != null)
             {
                 mainWindow.Content = loginPage;
+            }
+        }
+
+        //Add product to db
+        private void AddProductToDatabase(string productName, decimal price, int quantity, DateTime expiryDate)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["PostgreSqlConnection"].ConnectionString;
+
+            try
+            {
+                using (var connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (var command = new NpgsqlCommand("INSERT INTO product (product_name, description, price, quantity_available, expiry_date) VALUES (@name, @desc, @price, @quantity, @expiryDate)", connection))
+                    {
+                        command.Parameters.AddWithValue("@name", productName);
+                        command.Parameters.AddWithValue("@price", price);
+                        command.Parameters.AddWithValue("@quantity", quantity);
+                        command.Parameters.AddWithValue("@expiryDate", expiryDate);
+
+                        int rowsAffected = command.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Product added successfully!", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Failed to add product.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to add product: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
     }
