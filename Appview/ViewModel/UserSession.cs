56d7b3a846a034e3ef79384db5fdb8464eb662ba
sulaimanfawwazak <sulaimanfawwazak@gmail.com>
2024-11-17 05:47:36 +0700

    Feat: add new file to get the user id based on the session

---
+++ Appview/ViewModel/UserSession.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appview.ViewModel
+{
+    public static class UserSession
+    {
+        private static int _userId;
+        public static int UserId
+        {
+            get { return _userId; }
+            set { _userId = value; }
+        }
+    }
+}
