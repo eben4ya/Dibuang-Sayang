f83d1600d92fd0179eaaf1e41133bc5bdff200a1
eben4ya <benaya.imanuela@gmail.com>
2024-11-17 23:24:21 +0700

    <feat: added constructor to get all orders from db at GetOrdersFromDB view model>

--- Appview/ViewModel/GetOrdersFromDB.cs
+++ Appview/ViewModel/GetOrdersFromDB.cs
@@ -14,18 +14,25 @@ namespace Appview.ViewModel
 {
     public class GetOrdersFromDB
     {
-        int userId = UserSession.UserId;
-
         string connectionString = ConfigurationManager.ConnectionStrings["PostgreSqlConnection"].ConnectionString;
 
-        public ObservableCollection<Reservation> Orders { get; set; }   
+        public ObservableCollection<Reservation> Orders { get; set; }
 
+        // Constructor to get orders for a specific user
         public GetOrdersFromDB(int userId)
         {
             Orders = new ObservableCollection<Reservation>();
             FetchOrders(userId);
         }
 
+        // Constructor to get all orders
+        public GetOrdersFromDB()
+        {
+            Orders = new ObservableCollection<Reservation>();
+            FetchAllOrders();
+        }
+
+        // Fetch orders for a specific user
         private void FetchOrders(int userId)
         {
             using (var connection = new NpgsqlConnection(connectionString))
@@ -57,6 +64,57 @@ namespace Appview.ViewModel
                 {
                     command.Parameters.AddWithValue("@UserId", userId);
 
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Orders.Add(new Reservation
+                            {
+                                ReservationID = (int)reader["reservation_id"],
+                                UserID = (int)reader["user_id"],
+                                ProductID = (int)reader["product_id"],
+                                ReservationDate = Convert.ToDateTime(reader["reservationdate"]),
+                                Status = reader["status"].ToString(),
+                                HotelID = (int)reader["hotel_id"],
+                                AmountPcs = (int)reader["amount_pcs"],
+                                TotalPrice = (decimal)reader["total_price"],
+                                HotelName = reader["hotel_name"].ToString(),
+                                ProductName = reader["productname"].ToString()
+                            });
+                        }
+                    }
+                }
+            }
+        }
+
+        // Fetch all orders without filtering by user
+        private void FetchAllOrders()
+        {
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                var query = @"
+                    SELECT 
+                        r.reservation_id,
+                        r.user_id,
+                        r.product_id,
+                        r.reservationdate,
+                        r.status,
+                        r.hotel_id,
+                        r.amount_pcs,
+                        r.total_price,
+                        h.hotel_name,
+                        p.productname
+                    FROM 
+                        reservation r
+                    JOIN 
+                        apps_hotel_modified h ON r.hotel_id = h.hotel_id
+                    JOIN 
+                        product p ON r.product_id = p.product_id";
+
+                using (var command = new NpgsqlCommand(query, connection))
+                {
                     using (var reader = command.ExecuteReader())
                     {
                         while (reader.Read())
