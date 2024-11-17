7de22b3187e8c249eca6595f0947da1399b762df
eben4ya <benaya.imanuela@gmail.com>
2024-11-18 00:32:01 +0700

    <feat: added update order status view model>

---
+++ Appview/ViewModel/UpdateOrder.cs
@@ -0,0 +1,53 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Appview.ViewModel
+{
+   public class UpdateOrder
+    {
+        private string connectionString;
+
+        public UpdateOrder()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["PostgreSqlConnection"].ConnectionString;
+        }
+
+        public void UpdateOrderStatus(int reservationId, string status)
+        {
+            try
+            {
+                using (var connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "UPDATE reservation SET status = @status WHERE reservation_id = @reservationId";
+
+                    using (var command = new NpgsqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@status", status);
+                        command.Parameters.AddWithValue("@reservationId", reservationId);
+
+                        int rowsAffected = command.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Order status updated successfully!", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Failed to update order status.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error updating order status: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
